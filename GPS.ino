3f783862366ff3143fc1b204ab23cc957cec104b
nachopank <31256272+nachopank@users.noreply.github.com>
2018-11-29 15:49:40 +0100

    Update GPS.ino

--- GPS.ino
+++ GPS.ino
@@ -1,5 +1,7 @@
 #include <SoftwareSerial.h>
 #include <String.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
 #define PMTK_SET_NMEA_OUTPUT "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29"// "$PMTK220, 10000*2F" //Once every 10 s, 100 mhz
  
@@ -15,7 +17,8 @@ const int ledPin = 13;
  int conta=0;
  int indices[13];
 
-
+ char lat[9];
+ char lon[9];
 
 
 SoftwareSerial gps(RX, TX);
@@ -88,11 +91,20 @@ void loop()
            }
            for (int j=indices[i];j<(indices[i+1]-1);j++){
              Serial.print(linea[j+1]); 
+             if (i == 2){
+             lat[j - indices[2]] = linea[j+1];
+             }
+             if (i == 4){
+             lon[j - indices[4]] = linea[j+1];
+             }
            }
            Serial.println("");
          }
          Serial.println("---------------");
        }
+       Serial.println(parsearLatitud(lat));
+       Serial.println("aaaaaaaaaaaaaaaaaa");
+       Serial.println(parsearLongitud(lon));
        conta=0;                    // Reset the buffer
        for (int i=0;i<300;i++){    //  
          linea[i]=' ';             
@@ -100,4 +112,53 @@ void loop()
      }
    }
  }
+}
+double parsearLatitud(const char *latitud)
+{
+       unsigned char dd[2];
+       unsigned char mm[7];
+       double decimal1, decimal2;
+
+       double coordenada, latitudParseada;
+       int grados;
+       double minutos;
+
+       coordenada = atof(latitud);
+       grados = (int) floor(coordenada / 100);
+       minutos = fmod(coordenada, 100);
+       minutos = minutos / 60;
+       latitudParseada = grados + minutos;
+
+       //decimal2 = (float) decimal2 / 60;
+
+       //decimal1 = (float) decimal1 + decimal2;
+
+       //return decimal1;
+       return latitudParseada;
+}
+
+float parsearLongitud(const char *longitud)
+{
+       double longitudParseada;
+       unsigned char dd[3]; // = "";
+       unsigned char mm[7]; // = "";
+       int p;
+       double coordenada;
+       int grados;
+       double minutos;
+
+       double decimal1, decimal2;
+
+       coordenada = atof(longitud);
+       grados = (int) floor(coordenada / 100);
+       minutos = fmod(coordenada, 100);
+       minutos = minutos / 60;
+       longitudParseada = grados + minutos;
+
+       //decimal2 = (float) decimal2 / 60;
+
+       //decimal1 = (float) decimal1 + decimal2;
+
+       //return decimal1;
+       return longitudParseada;
 }
